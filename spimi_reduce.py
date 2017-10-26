299bc262fb3301b76a7ca9bf2e2945876a7c965c
34ndju <34ndju@gmail.com>
2017-10-26 17:34:59 -0500

    map reduce spimi

--- spimi_reduce.py
+++ spimi_reduce.py
@@ -1,13 +1,12 @@
-test_tokens = [
-{"term": "apple", "docID": 1},
-{"term": "insatiable", "docID": 1},
-{"term": "apple", "docID": 2},
-{"term": "herring", "docID": 2},
-{"term": "incredulous", "docID": 3},
-{"term": "insatiable", "docID": 4}
-]
+def spimi_map(collection):
+    # docID is associated with spot in array
+    mapped_tokens = []
+    for i, doc in enumerate(collection):
+        terms = doc.split()
+        for term in terms:
+            mapped_tokens.append({"term":term, "docID": i})
+    return mapped_tokens
 
-# previously mapped
 
 def spimi_reduce(tokens):
     output_file = {}
@@ -15,7 +14,7 @@ def spimi_reduce(tokens):
 
     token_index = 0
     memory_used = 0
-    max_memory = 35
+    max_memory = 70
 
     while memory_used < max_memory and token_index < len(tokens):
         token = tokens[token_index]
@@ -30,5 +29,24 @@ def spimi_reduce(tokens):
         token_index+=1
     return dictionary
 
-print spimi_reduce(test_tokens)
-# {'insatiable': [1, 4], 'herring': [2], 'apple': [1, 2], 'incredulous': [3]}
+collection = []
+collection.append("injustice anywhere is an injustice everywhere")
+collection.append("the time is always right to do what is right")
+collection.append("we must build dikes of courage to hold back the flood of fear")
+collection.append("seeking is not always believing")
+collection.append("we must use time creatively")
+collection.append("a lie cannot live")
+collection.append("a right delayed is a right denied")
+
+mapped_collection = spimi_map(collection)
+
+print spimi_reduce(mapped_collection)
+
+'''
+{'right': [1, 1, 6, 6], 'is': [0, 1, 1, 3, 6], 'back': [2], 'an': [0],
+'fear': [2], 'lie': [5], 'what': [1], 'to': [1, 2], 'flood': [2], 'live': [5],
+'believing': [3], 'injustice': [0, 0], 'denied': [6], 'creatively': [4], 'build': [2],
+'do': [1], 'we': [2, 4], 'seeking': [3], 'time': [1, 4], 'use': [4], 'delayed': [6],
+'courage': [2], 'everywhere': [0], 'not': [3], 'hold': [2], 'must': [2, 4], 'a': [5, 6, 6],
+'always': [1, 3], 'dikes': [2], 'anywhere': [0], 'cannot': [5], 'of': [2, 2], 'the': [1, 2]}
+'''
